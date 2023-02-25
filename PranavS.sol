ca38ad64954ab7d9492d30c2c7f3cf4155a22796
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-02-25 17:56:21 +0530

    added smart contract

--- PranavS.sol
+++ PranavS.sol
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
-import "@openzeppelin/contracts@4.8.1/utils/Counters.sol";
+import "./.deps/npm/@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
+import "./.deps/npm/@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./.deps/npm/@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+import "./.deps/npm/@openzeppelin/contracts@4.8.1/utils/Counters.sol";
 
 contract PranavS is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
