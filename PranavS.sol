973cb4d4a3d22b119246ae597dfe250a6bbf6853
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-02-25 17:29:22 +0530

    added smart contract

---
+++ PranavS.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.1/utils/Counters.sol";
+
+contract PranavS is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("PranavS", "PvS") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://pranavsalunkhe.vercel.app/tokens";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
